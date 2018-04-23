7c2287f58aa74c597e364830c810fe80e5e831cc
unknown <buxiri@hotmail.com>
2018-04-23 00:22:30 -0300

    Teclado // Logo animado // Lista flat // Lista de item // Tela de opções // Tela de Temas

--- app/index.js
+++ app/index.js
@@ -1,20 +1,18 @@
+import React from 'react';
 import EStyleSheet from 'react-native-extended-stylesheet';
-import React, { Component } from 'react';
 
-import Home from './screens/Home';
-
-export default class App extends Component {
-  render() {
-    return (
-      <Home />
-    );
-  }
-}
+import Themes from './screens/Themes';
 
 EStyleSheet.build({
   $primaryBlue: '#4F6D7A',
+  $primaryOrange: '#D57A66',
+  $primaryGreen: '#00BD9D',
+  $primaryPurple: '#9E768F',
   $white: '#FFFFFF',
   $lightGray: '#F0F0F0',
   $border: '#979797',
   $inputText: '#797979',
+  $darkText: '#343434',
 });
+
+export default () => <Themes />;
